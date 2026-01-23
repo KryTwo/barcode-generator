96836f3660fa620bbea0e74f1078f9c9ddf2ef96
KryTwo <krytwo@yahoo.com>
2026-01-23 14:49:34 +0300

    Добавлены метаданные файла, обновлен ридми

--- main.go
+++ main.go
@@ -15,9 +15,9 @@ func main() {
 
 	config.Init()
 	cfg := config.Get()
-	myApp := fyneApp.NewWithID("bcgen.myapp")
+	myApp := fyneApp.NewWithID("com.krytwo.vi-helper.bcvigen")
 	myApp.Settings().SetTheme(theme.DarkTheme())
-	window := myApp.NewWindow("Barcode Generator")
+	window := myApp.NewWindow("VI Barcode Helper")
 	controller := app.NewController(cfg)
 	gui.MakeUI(window, controller, myApp)
 
